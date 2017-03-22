2cfc3aad1451ff4189bcf9a939f98f912634a7e3
Kateacular <kate.orr@detroitlabs.com>
2017-03-22 16:13:26 -0400

    adding clothing items array

--- fashion.js
+++ fashion.js
@@ -1,2 +1,30 @@
+// - Create an inventory application for a retail store
+// - Each item in the inventory should be represented by an object, 
+// which should store 
+// a description of the item, 
+// the price of the item, 
+// and the number of items in stock
+// - Users should be able to 
+// add an item, 
+// delete an item, 
+// search for an item, 
+// modify the number of items available, 
+// and change an item’s description
+
+// ***********
+
+
+
+
+
+
 var inventory = [];
+
+
+
+var clothingItems = function(type,price,stock){
+	this.type = type;
+	this.price = price;
+	this.stock = stock;
+};
 
